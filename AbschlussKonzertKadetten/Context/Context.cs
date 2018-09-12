2912b2d715b64e2e322d51dbb16dbeaf4da90d78
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-09-12 09:30:44 +0200

    add conntext and delete git ignore (dublicated)

---
+++ AbschlussKonzertKadetten/Context/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbschlussKonzertKadetten.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbschlussKonzertKadetten.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Order { get; set; }
+    }
+}
