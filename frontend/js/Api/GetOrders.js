0f26082fcc693fe473328ed741885e850b54c6cc
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-09-18 16:39:46 +0200

    delete frontend folder

--- frontend/js/Api/GetOrders.js
+++
@@ -1,8 +0,0 @@
-function GetOrders() {
-    console.log("Get")
-    fetch(uri)
-    .then(res => res.json())//response type
-    .then(data => console.log(data)); //log the data;
-    return false
-
-}
