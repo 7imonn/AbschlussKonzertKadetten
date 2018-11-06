71f5945b26270f91c75647a2f2b4e1d3981bf708
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-11-06 16:34:14 +0100

    delete other function

--- AbschlussKonzertKadetten/wwwroot/admin/js/backend-excel.js
+++ AbschlussKonzertKadetten/wwwroot/admin/js/backend-excel.js
@@ -1,20 +1,3 @@
-// var tableToExcel = (function () {
-//     var uri = 'data:application/vnd.ms-excel;charset=UTF-8;base64,',
-//         template =
-//             '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-//         base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))); },
-//         format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }); };
-
-//     // while (html.indexOf('ä') != -1) html = html.replace('ä', '&auml;');
-//     // while (html.indexOf('ö') != -1) html = html.replace('ö', '&ouml;');
-//     // while (html.indexOf('ü') != -1) html = html.replace('ü', '&uuml;');
-
-//     return function (table, name) {
-//         if (!table.nodeType) table = document.getElementById(table);
-//         var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML };
-//         window.location.href = uri + base64(format(template, ctx));
-//     };
-// })();
 function tableToExcel() {
     var table = document.getElementById('reservationenTable');
     var html = table.outerHTML;
