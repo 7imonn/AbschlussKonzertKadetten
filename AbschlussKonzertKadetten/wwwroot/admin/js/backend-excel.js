22f777af401e545a9e19acebc18d78e6f2d65d4d
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-12-10 10:35:11 +0100

    add &eacute; to excel

--- AbschlussKonzertKadetten/wwwroot/admin/js/backend-excel.js
+++ AbschlussKonzertKadetten/wwwroot/admin/js/backend-excel.js
@@ -2,9 +2,11 @@ function tableToExcel() {
     var table = document.getElementById('reservationenTable');
     var html = table.outerHTML;
 
-    while (html.indexOf('ä') != -1) html = html.replace('ä', '&auml;');
-    while (html.indexOf('ö') != -1) html = html.replace('ö', '&ouml;');
-    while (html.indexOf('ü') != -1) html = html.replace('ü', '&uuml;');
+    while (html.indexOf('ä') !== -1) html = html.replace('ä', '&auml;');
+    while (html.indexOf('ö') !== -1) html = html.replace('ö', '&ouml;');
+    while (html.indexOf('ü') !== -1) html = html.replace('ü', '&uuml;');
+    while (html.indexOf('é') !== -1) html = html.replace('é', '&eacute;');
+    while (html.indexOf('ë') !== -1) html = html.replace('ë', '&euml;');
 
     window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
 };
